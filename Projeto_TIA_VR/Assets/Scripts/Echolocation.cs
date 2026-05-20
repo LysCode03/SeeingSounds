ce4d313102e5125500075d8ee2b151fd45488f5d
Lys <joanapddeus@gmail.com>
2026-05-20 11:52:32 +0100

    Name correction ecolocation -> echolocation

--- Projeto_TIA_VR/Assets/Scripts/Echolocation.cs
+++ Projeto_TIA_VR/Assets/Scripts/Echolocation.cs
@@ -1,28 +1,28 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Ecolocation : MonoBehaviour
+public class Echolocation : MonoBehaviour
 {
-    public GameObject ecolocationLight;
+    public GameObject echolocationLight;
     public InputActionReference openMenuAction;
 
     public void Awake()
     {
         openMenuAction.action.Enable(); // the input system acts weird if we don't activate and disable, at respective times
-        openMenuAction.action.performed += ActivateEcolocation;
+        openMenuAction.action.performed += ActivateEcholocation;
         InputSystem.onDeviceChange += OnDeviceChange; // in case, for exmaple, the controller is disconnected
     }
 
     private void OnDestroy()
     {
         openMenuAction.action.Disable();
-        openMenuAction.action.performed -= ActivateEcolocation;
+        openMenuAction.action.performed -= ActivateEcholocation;
         InputSystem.onDeviceChange -= OnDeviceChange;
     }
 
-    private void  ActivateEcolocation(InputAction.CallbackContext context)
+    private void  ActivateEcholocation(InputAction.CallbackContext context)
     {
-        ecolocationLight.SetActive(!ecolocationLight.activeSelf);
+        echolocationLight.SetActive(!echolocationLight.activeSelf);
     }
 
     private void OnDeviceChange(InputDevice device, InputDeviceChange change)
@@ -31,11 +31,11 @@ public class Ecolocation : MonoBehaviour
         {
             case InputDeviceChange.Disconnected:
                 openMenuAction.action.Disable();
-                openMenuAction.action.performed -= ActivateEcolocation;
+                openMenuAction.action.performed -= ActivateEcholocation;
                 break;
             case InputDeviceChange.Reconnected:
                  openMenuAction.action.Enable();
-                 openMenuAction.action.performed += ActivateEcolocation;
+                 openMenuAction.action.performed += ActivateEcholocation;
                  break;
         }
     }
